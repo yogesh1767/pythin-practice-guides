31a3fe808d5e362f44b26a72c113587a9a557ff2
Yogeshraja-python <56289404+Yogeshraja-python@users.noreply.github.com>
2022-01-30 11:11:56 -0500

    variable name changed

--- factorial.py.py
+++ factorial.py.py
@@ -1,38 +1,13 @@
 
 num = int(input("enter a number: "))
  
-fac = 1
+fact = 1
 i = 1
  
 while (i <= num):
-    fac = fac * i
+    fact = fact * i
     #print (fac)
     i = i + 1
     #print(i)
  
-print("factorial of ", num, " is ", fac)
-
-
-#factorial program using opps
-class factorial:
-    num=int(input('enter the number: '))
-    def facto(self):
-        fact=1
-        for i in range(self.num,0,-1):
-            fact=fact*i
-        print(fact)
-obj=factorial()
-obj.facto()
-
-#number passing arguments
-class factorial:
-
-    def facto(self,a):
-        self.num=a
-        fact=1
-        for i in range(1,self.num+1):
-            fact=fact*i
-        print(fact)
-obj=factorial()
-obj.facto(5)
-
+print("factorial of ", num, " is ", fact)
